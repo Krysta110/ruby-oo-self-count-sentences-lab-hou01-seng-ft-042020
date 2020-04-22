5da4efa9621291497dbd48425bdab6ceef214563
Krysta110 <62581900+Krysta110@users.noreply.github.com>
2020-04-22 13:54:15 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self.end_with?(".")
   end
 
   def question?
-
+    return self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
 
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
+
 end
